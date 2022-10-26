f8f8203f558f64b6b4dfb7eb86613ab5f31c3899
hongkhanh842 <1851120019@sv.ut.edu.vn>
2022-10-26 18:41:34 +0700

    get catagory

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -14,8 +14,8 @@
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Blank Page</li>
+                            <li class="breadcrumb-item"><a href="/admin">Home</a></li>
+                            <li class="breadcrumb-item active">Category List</li>
                         </ol>
                     </div>
                 </div>
@@ -25,28 +25,54 @@
         <!-- Main content -->
         <section class="content">
 
-            <!-- Default box -->
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Title</h3>
-
-                    <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                            <i class="fas fa-minus"></i></button>
-                        <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-                            <i class="fas fa-times"></i></button>
-                    </div>
+                    <h3 class="card-title">Category List</h3>
                 </div>
+                <!-- /.card-header -->
                 <div class="card-body">
-                    Start creating your amazing application!
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th style="width: 10px">ID</th>
+                            <th>Title</th>
+                            <th>Keywords</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Status</th>
+                            <th style="width: 40px">Edit</th>
+                            <th style="width: 40px">Delete </th>
+                            <th style="width: 40px">Show </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($data as $rs)
+                        <tr>
+                            <td>{{$rs->id}}</td>
+                            <td>{{$rs->title}}</td>
+                            <td>{{$rs->keywords}}</td>
+                            <td>{{$rs->description}}</td>
+                            <td>{{$rs->image}}</td>
+                            <td>{{$rs->status}}</td>
+                            <td><a href="/admin/category/edit{{$rs->id}}" class="btn btn-block btn-success btn-sm">Edit</a></td>
+                            <td><a href="/admin/category/delete{{$rs->id}}" class="btn btn-block btn-danger btn-sm">Delete</a></td>
+                            <td><a href="/admin/category/show{{$rs->id}}" class="btn btn-block btn-info btn-sm">Show</a></td>
+                        </tr>
+                        </tbody>
+                        @endforeach
+                    </table>
                 </div>
                 <!-- /.card-body -->
-                <div class="card-footer">
-                    Footer
+                <div class="card-footer clearfix">
+                    <ul class="pagination pagination-sm m-0 float-right">
+                        <li class="page-item"><a class="page-link" href="#">«</a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">»</a></li>
+                    </ul>
                 </div>
-                <!-- /.card-footer-->
             </div>
-            <!-- /.card -->
 
         </section>
         <!-- /.content -->
