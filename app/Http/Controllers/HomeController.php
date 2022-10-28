aef59b28b81d7ad6ec515ff07d0b31264dbaae91
hongkhanh842 <1851120019@sv.ut.edu.vn>
2022-10-28 21:25:03 +0700

    Home Slither

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $sliderdata = Product::limit(4)->get();
+        /*dd($sliderdata);*/
+        return view('home.index',[
+            'sliderdata' => $sliderdata,
+        ]);
     }
 }
