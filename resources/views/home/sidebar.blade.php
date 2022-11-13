b903eadbf45bd0944f3ebf6f2a74c04dd24d578e
hongkhanh842 <1851120019@sv.ut.edu.vn>
2022-11-13 18:55:33 +0700

    Fix temp front

--- resources/views/home/sidebar.blade.php
+++ resources/views/home/sidebar.blade.php
@@ -4,24 +4,26 @@
     <div class="container">
         <div id="responsive-nav">
             @php
-            $mainCategories = \App\Http\Controllers\HomeController::maincategoryList();
+                $mainCategories = \App\Http\Controllers\HomeController::maincategorylist()
             @endphp
-            <!-- category nav -->
-            <div class="category-nav" @if (!@isset($page)) show-on-click  @endif>
+                <!-- category nav -->
+            <div class="category-nav @if (!@isset($page)) show-on-click @endif">
                 <span class="category-header">Categories <i class="fa fa-list"></i></span>
                 <ul class="category-list">
 
                     @foreach($mainCategories as $rs)
-                    <li class="dropdown side-dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">{{$rs->title}}<i class="fa fa-angle-right"></i></a>
-                        <div class="custom-menu">
-                            <div class="row">
-                                @if(count($rs->children))
-                                    @include('home.categorytree',['children'=>$rs->children])
-                                @endif
+                        <li class="dropdown side-dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">{{ $rs->title }}<i class="fa fa-angle-right"></i></a>
+                            <div class="custom-menu">
+                                <div class="row">
+
+                                    @if(count($rs->children))
+                                        @include('home.categorytree',['children' => $rs->children])
+                                    @endif
+
+                                </div>
                             </div>
-                        </div>
-                    </li>
+                        </li>
                     @endforeach
                 </ul>
             </div>
@@ -31,13 +33,13 @@
             <div class="menu-nav">
                 <span class="menu-header">Menu <i class="fa fa-bars"></i></span>
                 <ul class="menu-list">
+
                     <li><a href="{{route('home')}}">Home</a></li>
-                    <li><a href="{{route('about')}}">About Us</a></li>
+                    <li><a href="{{route('about')}}">About </a></li>
                     <li><a href="{{route('references')}}">References</a></li>
-                    <li><a href="#">Shop</a></li>
-                    <li class="dropdown mega-dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Women <i class="fa fa-caret-down"></i></a>
+                   {{-- <li class="dropdown mega-dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Women <i class="fa fa-caret-down"></i></a>
                         <div class="custom-menu">
-                            <div class="row">
+                           --}}{{-- <div class="row">
                                 <div class="col-md-4">
                                     <ul class="list-links">
                                         <li>
@@ -50,30 +52,7 @@
                                     </ul>
                                     <hr class="hidden-md hidden-lg">
                                 </div>
-                                <div class="col-md-4">
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">Categories</h3></li>
-                                        <li><a href="#">Women’s Clothing</a></li>
-                                        <li><a href="#">Men’s Clothing</a></li>
-                                        <li><a href="#">Phones & Accessories</a></li>
-                                        <li><a href="#">Jewelry & Watches</a></li>
-                                        <li><a href="#">Bags & Shoes</a></li>
-                                    </ul>
-                                    <hr class="hidden-md hidden-lg">
-                                </div>
-                                <div class="col-md-4">
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">Categories</h3></li>
-                                        <li><a href="#">Women’s Clothing</a></li>
-                                        <li><a href="#">Men’s Clothing</a></li>
-                                        <li><a href="#">Phones & Accessories</a></li>
-                                        <li><a href="#">Jewelry & Watches</a></li>
-                                        <li><a href="#">Bags & Shoes</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                            </div>--}}{{--
                             <div class="row hidden-sm hidden-xs">
                                 <div class="col-md-12">
                                     <hr>
@@ -87,102 +66,7 @@
                                 </div>
                             </div>
                         </div>
-                    </li>
-                    <li class="dropdown mega-dropdown full-width"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Men <i class="fa fa-caret-down"></i></a>
-                        <div class="custom-menu">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="{{asset('assets')}}/img/banner06.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">Women’s</h3>
-                                            </div>
-                                        </a>
-                                        <hr>
-                                    </div>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">Categories</h3></li>
-                                        <li><a href="#">Women’s Clothing</a></li>
-                                        <li><a href="#">Men’s Clothing</a></li>
-                                        <li><a href="#">Phones & Accessories</a></li>
-                                        <li><a href="#">Jewelry & Watches</a></li>
-                                        <li><a href="#">Bags & Shoes</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="{{asset('assets')}}/img/banner07.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">Men’s</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <hr>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">Categories</h3></li>
-                                        <li><a href="#">Women’s Clothing</a></li>
-                                        <li><a href="#">Men’s Clothing</a></li>
-                                        <li><a href="#">Phones & Accessories</a></li>
-                                        <li><a href="#">Jewelry & Watches</a></li>
-                                        <li><a href="#">Bags & Shoes</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="{{asset('assets')}}/img/banner08.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">Accessories</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <hr>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">Categories</h3></li>
-                                        <li><a href="#">Women’s Clothing</a></li>
-                                        <li><a href="#">Men’s Clothing</a></li>
-                                        <li><a href="#">Phones & Accessories</a></li>
-                                        <li><a href="#">Jewelry & Watches</a></li>
-                                        <li><a href="#">Bags & Shoes</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="{{asset('assets')}}/img/banner09.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">Bags</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <hr>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">Categories</h3></li>
-                                        <li><a href="#">Women’s Clothing</a></li>
-                                        <li><a href="#">Men’s Clothing</a></li>
-                                        <li><a href="#">Phones & Accessories</a></li>
-                                        <li><a href="#">Jewelry & Watches</a></li>
-                                        <li><a href="#">Bags & Shoes</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li><a href="#">Sales</a></li>
-                    <li class="dropdown default-dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Pages <i class="fa fa-caret-down"></i></a>
-                        <ul class="custom-menu">
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="products.html">Products</a></li>
-                            <li><a href="product-page.html">Product Details</a></li>
-                            <li><a href="checkout.html">Checkout</a></li>
-                        </ul>
-                    </li>
+                    </li>--}}
                     <li><a href="{{route('faq')}}">FAQ</a></li>
                     <li><a href="{{route('contact')}}">Contact</a></li>
                 </ul>
--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -7,11 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <title>@yield('title')</title>
-    <meta name="description" content="@yield('description')">
-   {{-- <meta name="description" content="@yield('title')">
-    <meta name="description" content="@yield('title')">--}}
-    <link rel="icon" type="image/x-icon" href="@yield('icon')">
+    <title>@yield("title")</title>
+    <meta name="description" content="@yield("description")">
+    <meta name="keywords" content="@yield("keywords")">
+    <meta name="author" content="Yuksel CELIK">
+    <link rel="icon" type="image/x-icon" href="@yield("icon")">
 
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
@@ -37,15 +37,16 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
     <![endif]-->
-    @yield('head')
+    @yield("head")
 </head>
 
 <body>
-@include('home.header')
+@include("home.header")
 
 @section('sidebar')
-    @include('home.sidebar')
+    @include("home.sidebar")
 @show
 
 @section('slider')
@@ -53,8 +54,7 @@
 
 @yield('content')
 
-@include('home.footer')
+<@include("home.footer")
 @yield('foot')
 </body>
-
 </html>
