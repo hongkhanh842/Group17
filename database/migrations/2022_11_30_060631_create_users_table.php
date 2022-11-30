0fe3de257a0720eab8fd4ec2a506eb887d1ed7af
hongkhanh842 <1851120019@sv.ut.edu.vn>
2022-11-30 22:59:47 +0700

    Auth

--- database/migrations/2022_11_30_060631_create_users_table.php
+++ database/migrations/2022_11_30_060631_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id()->autoIncrement();
             $table->string('name');
             $table->string('email');
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('avatar')->nullable();
             $table->string('address')->nullable();
             $table->timestamps();
---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ route('auth.redirect', 'github') }}">GITHUB</a>
+<br>
+<a href="{{ route('auth.redirect', 'facebook') }}">FACEBOOK</a>
