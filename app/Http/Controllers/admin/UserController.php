c7e903418472722efd8d93771672399d414b5643
hongkhanh842 <1851120019@sv.ut.edu.vn>
2022-12-09 09:26:16 +0700

    fix

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -12,10 +12,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = User::all();
-        return view('admin.user.index',[
-            'data' => $data,
-        ]);
+        return view('admin.user.index');
     }
 
     public function create()
